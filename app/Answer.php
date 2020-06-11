eb4a2d965c62166057ae8c312c8dbdcf728ced14
edcahie <edmondcahie@gmail.com>
2020-06-11 01:11:42 +0000

    Displaying answer for question

--- app/Answer.php
+++ app/Answer.php
@@ -29,4 +29,9 @@ class Answer extends Model
             $answer->question->increment('anwswers_count');
         });
     }
+
+    public function getCreatedDateAttribute(){
+
+        return $this->created_at->diffForHumans();
+    }
 }
