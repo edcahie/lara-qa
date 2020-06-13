c0301180e4d92c78dfb6ba7250d4a04959a9ade4
edcahie <edmondcahie@gmail.com>
2020-06-13 22:58:17 +0000

    Update best_answer_id on questions table when an answer removed usin

--- app/Answer.php
+++ app/Answer.php
@@ -30,8 +30,14 @@ class Answer extends Model
             $answer->question->increment('anwswers_count');
         });
 
-        static::deleted(function ($answer){
-            $answer->question->decrement('anwswers_count');
+        static::deleted(function ($answer) {
+
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if ($question->best_answer_id === $answer->id) {
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
     }
 
@@ -39,4 +45,10 @@ class Answer extends Model
 
         return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute(){
+
+        return $this->id  === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
+
 }
